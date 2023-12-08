4acce43318bc73fc14a3a4e29621e53eb865e0ab
WilsonG08 <wilson.guayanay@epn.edu.ec>
2023-12-08 10:53:56 -0500

    el boton modificar la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,9 @@
 saludar = function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo = document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./imagenes/pato.gif"
     console.log("ingresa a saludar")
+
+
+
 }
